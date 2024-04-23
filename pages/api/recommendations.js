51fad2ce43656624a2ca839b11bb1c6f35514391
Sir-Dommy <108345471+Sir-Dommy@users.noreply.github.com>
2024-04-23 17:01:01 +0300

    Update recommendations.js

--- pages/api/recommendations.js
+++ pages/api/recommendations.js
@@ -3,7 +3,7 @@ const recommendationCard = [
   {
     id: 0,
     name: 'Joseck Osugo',
-    image: "images/shahmir.jfif",
+    image: "images/osugo.jpg",
     designation: 'React JS | NEXT JS | Node | MySQL',
     view: "Hey everyone! I wanted to take a moment to recommend Dominick Kyengo for any Software Development opportunities. I had the pleasure of working with him on multiple projects where he showcased a strong understanding of Software Development concepts and delivered high-quality code. He consistently demonstrated a willingness to learn and grow, and his enthusiasm for tackling new challenges was contagious. Domimick is a reliable and dedicated team member who would be a great asset to any React.js team. Highly recommended! 👍! 🌟",
     linkednURL: "https://www.linkedin.com/in/shahmir-minhas/"
