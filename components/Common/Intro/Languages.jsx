bd189c9aec799fd3d855fa346d23a1afb4b691c8
Sir-Dommy <108345471+Sir-Dommy@users.noreply.github.com>
2024-04-23 17:15:34 +0300

    Update Languages.jsx

--- components/Common/Intro/Languages.jsx
+++ components/Common/Intro/Languages.jsx
@@ -16,15 +16,15 @@ const Languages = () => {
         }, 30);
 
         return () => clearInterval(timer);
-    }, [urdu, english])
+    }, [swahili, english])
     return (
         <div className="flex flex-col space-y-1 pt-6">
             <div className="flex flex-col gap-y-4">
                 <span className='text-Snow text-xs font-bold'>Languages</span>
                 <div className="flex flex-row items-center justify-center space-x-6">
                     <div className="flex flex-col items-center justify-center gap-y-2">
-                        <Progress strokeColor="#1fdf64" type="circle" percent={urdu} size={75} />
-                        <span className='text-xs font-bold text-Snow'>Urdu</span>
+                        <Progress strokeColor="#1fdf64" type="circle" percent={swahili} size={75} />
+                        <span className='text-xs font-bold text-Snow'>Swahili</span>
                     </div>
                     <div className="flex flex-col items-center justify-center gap-y-2">
                         <Progress strokeColor="#1fdf64" type="circle" percent={english} size={75} />
